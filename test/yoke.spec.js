d2f7a1b494c90fccbdb18eec26d63806a66e5841
Rytis Alekna <r.alekna@gmail.com>
2016-07-29 00:25:14 +0300

    implementation and tests

---
+++ test/yoke.spec.js
@@ -0,0 +1,77 @@
+/**
+ * Created by Rytis on 2016-07-28.
+ */
+var expect  = require('chai').expect;
+var sinon   = require('sinon');
+
+var yoke = require('../yoke');
+describe('yoke binder: ', function() {
+
+  it('should create bound function', function() {
+
+    function add(arg1) {
+      return this + arg1;
+    }
+
+    expect(yoke(add, 2)(2)).to.equal(4);
+
+  });
+
+  // binding support flag is initiated on yoke initialization, so there is no way to test it after it's loaded.
+  // But I tried it by setting it to dynamic check and it worked. Trust me :)
+  xit('should create bound function when native binding not supported (to run this case, yoke code has to be modified)', function() {
+
+    function add(arg1) {
+      return this + arg1;
+    }
+
+    var origBind = Function.prototype.bind;
+    Function.prototype.bind = undefined;
+
+    expect(yoke(add, 2)(2)).to.equal(4);
+    expect(Function.prototype.bind).to.be.equal(undefined);
+    Function.prototype.bind = origBind;
+
+  });
+
+  it('should reuse bound function', function() {
+
+    function add(arg1) {
+      return this + arg1;
+    }
+
+    var boundAdd2_1 = yoke(add, 2);
+    var boundAdd2_2 = yoke(add, 2);
+
+    expect(boundAdd2_1).to.be.equal(boundAdd2_2);
+
+  });
+
+  it('should warn if global object or `undefined` is used as context', function() {
+
+    function Some() {
+      return this;
+    }
+
+    sinon.spy(console, 'log');
+
+    var yokeInstance = yoke(Some, undefined);
+
+    // expect(()).to.equal(undefined);
+    // console.log('hey');
+    expect(console.log.calledOnce).to.be.true;
+    expect(console.log.getCall(0).args[0]).to.match(/^WARNING/i);
+
+  });
+
+  xit('should create yoke instance for restricted context', function() {
+
+    function SomeClass() {
+
+    }
+
+    sinon.spy(console, 'log');
+
+  });
+
+});
---
+++ yoke.js
@@ -0,0 +1,133 @@
+/**
+ * Created by Rytis Alekna (r.alekna@gmail.com) on 2016-07-20.
+ */
+"use strict";
+var yoke = (function() {
+
+  /**
+   * @typedef {Array} Binding
+   * @property  0 - context
+   * @property  1 - function
+   * @property  2 - supplied data
+   * @property  3 - bound function
+   */
+
+  yoke.CONTEXT_IS_FUNCTION_WARNING           = true;
+  yoke.CONTEXT_IS_FUNCTION_EXCEPTION         = false;
+  yoke.CONTEXT_IS_GLOBAL_OBJECT_WARNING      = true;
+  yoke.CONTEXT_IS_GLOBAL_OBJECT_EXCEPTION    = false;
+
+  var FUNCTION_BINDING_SUPPORTED = (typeof Function.prototype.bind === 'function');
+
+  var globalStorage = [];
+
+  /**
+   * @private
+   * @return {Binding[]}
+   */
+  function getStorage() {
+    return globalStorage;
+  }
+
+  /**
+   * @private
+   * @param storage
+   * @param context
+   * @param fn
+   * @param data
+   * @return {Binding}
+   */
+  function getBinding(storage, context, fn, data) {
+    return storage.filter(function filterStorageItem(item) {
+      return item[0] === context && item[1] === fn; // && item[2] === data;
+    })[0];
+  }
+
+  /**
+   * @private
+   * @param storage
+   * @param context
+   * @param fn
+   * @param data
+   * @return {*}
+   */
+  function createBinding(storage, context, fn, data) {
+    var binding;
+    var boundFn;
+    if (binding = getBinding(storage, context, fn, data)) {
+      return binding[3];
+    }
+    if (FUNCTION_BINDING_SUPPORTED) {
+      boundFn = fn.bind.apply(fn, [context].concat(data));
+    } else {
+      boundFn = function boundFunction() {
+        return fn.apply(context, data.concat(Array.prototype.slice.apply(arguments)));
+      }
+    }
+    storage.push([context, fn, data, boundFn]);
+    return boundFn;
+  }
+
+  function yoke(fnOrContext, context) {
+    var fn;
+
+    if (typeof fnOrContext === 'undefined') {
+      throw new Error('At least one argument must be specified!');
+    }
+
+    if (isGlobalObject(context)) {
+      var message = 'Specified context is global object!';
+      if (yoke.CONTEXT_IS_GLOBAL_OBJECT_WARNING) {
+        console.log('WARNING: ' + message);
+      }
+      if (yoke.CONTEXT_IS_GLOBAL_OBJECT_EXCEPTION) {
+        throw new Error(message);
+      }
+    }
+
+    if (typeof context === 'undefined') {
+      context = fnOrContext;
+      // there might be a situations where in EcmaScript "strict mode" supplied `this` as context points to `undefined`, `window` or `global`
+      if (typeof context === 'function') {
+        var message = 'Specified context is type of function!';
+        if (yoke.CONTEXT_IS_FUNCTION_WARNING) {
+          console.log('WARNING: ' + message);
+        }
+        if (yoke.CONTEXT_IS_FUNCTION_EXCEPTION) {
+          throw new Error(message);
+        }
+      }
+    } else {
+      fn = fnOrContext;
+    }
+
+    var data = Array.prototype.slice.call(arguments, 2);
+
+    if (fn) {
+      return createBinding(getStorage(), context, fn, data);
+    }
+
+    if (this instanceof yoke) {
+      var storage = [];
+
+      this.bind = function bind(fn) {
+        return createBinding(storage, context, fn, Array.prototype.slice.call(arguments, 1));
+      };
+
+      this.clear = function clear() {
+        storage.length = 0;
+      };
+
+    } else {
+      return new yoke(context);
+    }
+  }
+
+  return yoke;
+
+})();
+module.exports = yoke;
+
+function isGlobalObject(object) {
+  return object && (object.window === object || (object.global === object) || (typeof object.setInterval === 'function'));
+}
